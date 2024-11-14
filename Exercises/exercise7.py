3e1a0790bea23065f123d8d7b1f20535af5045fd
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-11-14 19:17:51 +0400

    Exercise 7 complete

---
+++ Exercises/exercise7.py
@@ -0,0 +1,14 @@
+for num in range(0, 51, 1):
+    print(num)
+
+for num in range(50, -1, -1):
+    print(num)
+
+for num in range(30, 51, 1):
+    print(num)
+
+for num in range(50, 9, -2):
+    print(num)
+
+for num in range(100, 201, 5):
+    print(num)
