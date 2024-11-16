f245de1539b258276f4de70c0f907b7202b3f896
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-11-16 23:32:27 +0400

    Update exercise8.py

--- Exercises/exercise8.py
+++ Exercises/exercise8.py
@@ -1,9 +1,15 @@
+# Define a list of names
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
+# Prompt the user to enter a name to search for in the list
 specified_name = input("Enter the name you want to search for: ")
 
+# Check if the entered name is in the 'names' list
 if specified_name in names:
+# If the name is found in the list, print a success message
     print(f"{specified_name} was found in the list!")
 else:
+# If the name is not found in the list, print a failure message
     print(f"{specified_name} was not found in the list.")
+
 
