00f380c6b6dddd1100ad01a211cf422f8f08d01d
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-11-16 23:21:28 +0400

    Update exercise2.py

--- Exercises/exercise2.py
+++ Exercises/exercise2.py
@@ -1,4 +1,9 @@
+# Assign the corresponding values to the variables
 sum1 = 8
 sum2 = 10
+
+# Add sum1 and sum2 together and store the result in the variable sum
 sum = (sum1 + sum2)
+
+# Print the value of the variable sum, which is the result of sum1 + sum2
 print(sum)
