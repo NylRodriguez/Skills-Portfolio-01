b784a405d884de644e470832b1cefae76d87cf8d
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-11-16 23:18:20 +0400

    Update exercise1.py

--- Exercises/exercise1.py
+++ Exercises/exercise1.py
@@ -1,5 +1,7 @@
+# Assign the corresponding values for each variable 
 word1 = "Coding"
 word2 = "is"
 word3 = "Cool"
 
+# Concatenate the three varialbles (word1, word2, word3) with spaces in between and print the result
 print(word1 + " " + word2 + " " + word3)
