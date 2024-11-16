b473b92ffecff5d6b6c263d0eeaaebe12c5d2b57
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-11-16 23:31:43 +0400

    Update exercise7.py

--- Exercises/exercise7.py
+++ Exercises/exercise7.py
@@ -1,14 +1,19 @@
+# First loop: Print numbers from 0 to 50 (inclusive), step 1
 for num in range(0, 51, 1):
-    print(num)
+    print(num)  # This will print numbers starting from 0 up to 50, one by one, incrementing by 1.
 
+# Second loop: Print numbers from 50 down to 0 (inclusive), step -1 (decrementing)
 for num in range(50, -1, -1):
-    print(num)
+    print(num)  # This will print numbers starting from 50 down to 0, decrementing by 1 on each iteration.
 
+# Third loop: Print numbers from 30 to 50 (inclusive), step 1
 for num in range(30, 51, 1):
-    print(num)
+    print(num)  # This will print numbers from 30 up to 50, incrementing by 1 each time.
 
+# Fourth loop: Print numbers from 50 down to 10 (inclusive), step -2 (decrement by 2)
 for num in range(50, 9, -2):
-    print(num)
+    print(num)  # This will print numbers starting from 50 and decrementing by 2 each time, down to 10.
 
+# Fifth loop: Print numbers from 100 to 200 (inclusive), step 5 (increment by 5)
 for num in range(100, 201, 5):
-    print(num)
+    print(num)  # This will print numbers from 100 up to 200, incrementing by 5 each time.
